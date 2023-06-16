a3717a9bfa5bf7e4f870b6f3d82f95a582cb9e48
Rokas Kasperavicius <goodname258@gmail.com>
2023-06-16 09:05:04 +0200

    Disabling the project :(

--- src/index.tsx
+++ src/index.tsx
@@ -13,20 +13,37 @@ import { store, persistor } from 'store'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 
+const MainProjectInstance = () => (
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <ToastContainer
+        hideProgressBar={true}
+        autoClose={3000}
+        position='bottom-right'
+      />
+
+      <Main />
+    </PersistGate>
+  </Provider>
+)
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <ToastContainer
-          hideProgressBar={true}
-          autoClose={3000}
-          position='bottom-right'
-        />
+    {/* <MainProjectInstance /> */}
 
-        <Main />
-      </PersistGate>
-    </Provider>
+    <div className='w-screen h-screen flex justify-center items-center flex-col gap-4'>
+      <div className='font-bold text-2xl'>The Project Has Been Disabled</div>
+      <div className='flex gap-2 text-xl'>
+        Contact the System Administrator:
+        <a
+          href='mailto:goodname258@gmail.com'
+          className='cursor-pointer italic'
+        >
+          goodname258@gmail.com
+        </a>
+      </div>
+    </div>
   </React.StrictMode>
 )
 
